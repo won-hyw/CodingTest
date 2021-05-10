3a5707e8ae5ef3727970c0b35f1d9d118bcfda9c
choi ji won <w2019@e-mirim.hs.kr>
2021-05-10 09:57:55 +0900

    펠린드롬 02 (반복문 일반화 최종)

--- 문제07-01.py
+++ 문제07-01.py
@@ -1,35 +1,9 @@
 #펠린드롬 간단하게 만들어보기
-## 모든 조건을 만족하지 못하면, 펠린드롬이 아니다
+#모든 조건을 만족하지 못하면, 펠린드롬이 아니다
 
 # 5글자인 경우
 a = "neven"
-a = "seven"
-if a[0] != a[4]:
-    print("펠린드롬이 아니다")
-elif a[1] != a[3]:
-    print("펠린드롬이 아니다")
-else:
-    print("펠린드롬이다.")
-
-# i값 0, 1
-for i in range(2):
-    if a[i] != a[4-i]:
+for i in range(len(a)//2):
+    if a[i] != a[len(a)-1-i]:
         print("펠린드롬이 아니다")
-    print("펠린드롬이다.")
-
-# 7글자인 경우
-a = "enevene"
-if a[0] != a[6]:
-    print("펠린드롬이 아니다")
-elif a[1] != a[5]:
-    print("펠린드롬이 아니다")
-elif a[2] != a[4]:
-    print("펠린드롬이 아니다")
-else:
-    print("펠린드롬이다.")
-
-# i는 0, 1, 2
-for i in range(3):
-    if a[i] != a[6-i]:
-        print("펠린드롬이 아니다")
-    print("펠린드롬이다.")
+print("펠린드롬이다")
