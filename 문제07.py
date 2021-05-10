44f87bd2a6aa43a6fc89adbae71cafea181642f3
choi ji won <w2019@e-mirim.hs.kr>
2021-05-10 09:16:17 +0900

    펠린드롬 연습

--- 문제07.py
+++ 문제07.py
@@ -1,14 +1,16 @@
-def solution(sentence):
-    str = ''
-    for c in sentence:
-        if c != '.' or c != ' ':
-            str += c
-    size = len(str)
-    for i in range(size // 2):
-        if str[i] != str[size - 1 - i]:
-            return False
-    return True
+# def solution(sentence):
+#     str = ''
+#     for c in sentence:
+#         if c != '.' or c != ' ':
+#             str += c
+#     size = len(str)
+#     for i in range(size // 2):
+#         if str[i] != str[size - 1 - i]:
+#             return False
+#     return True
 
+def solution(setence) :
+    pass
 
 # The following is code to output testcase. The code below is correct and you shall correct solution function.
 sentence1 = "never odd or even."
