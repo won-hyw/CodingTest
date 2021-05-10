80fdcb32b0232be1b698bffea6b2f8d068aa1be0
choi ji won <w2019@e-mirim.hs.kr>
2021-05-10 10:06:22 +0900

    문제07 풀이

--- 문제07.py
+++ 문제07.py
@@ -1,16 +1,13 @@
-# def solution(sentence):
-#     str = ''
-#     for c in sentence:
-#         if c != '.' or c != ' ':
-#             str += c
-#     size = len(str)
-#     for i in range(size // 2):
-#         if str[i] != str[size - 1 - i]:
-#             return False
-#     return True
-
-def solution(setence) :
-    pass
+def solution(sentence):
+    str = ''
+    for c in sentence:
+        if c != '.' and c != ' ':
+            str += c
+    size = len(str)
+    for i in range(size // 2):
+        if str[i] != str[size - 1 - i]:
+            return False
+    return True
 
 # The following is code to output testcase. The code below is correct and you shall correct solution function.
 sentence1 = "never odd or even."
