b7a21bc3675e6b5586951c144a7227da602d82ad
choi ji won <w2019@e-mirim.hs.kr>
2021-06-14 10:21:05 +0900

    01 음양더하기 4단계

--- 프로그래머스 Lv1/01 음양더하기.py
+++ 프로그래머스 Lv1/01 음양더하기.py
@@ -2,7 +2,7 @@ def solution(absolutes, signs):
     answer = 0
     # 넘겨받는 배열(list)의 길이를 일반화 시킴 len(absolutes)
     for i in range(len(absolutes)):
-        if signs[i] == True:
+        if signs[i] is True:
             answer += absolutes[i]
         else:
             answer -= absolutes[i]
